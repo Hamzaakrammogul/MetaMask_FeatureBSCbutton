771e909011bf28921eafb2197a760abf7ea19f00
hamzaakrammogul <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-09-13 20:19:33 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect } from 'react';
+import { ethers } from "ethers";
+function App() {
+useEffect(()=>{
+  const connect = async()=>{
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const res = await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner()
+  console.log(res);
+  }
+  connect();
+}, [])
+const blockExplorerUrl = "https://bscscan.com"
+const chainId = 56;
+const nodes = ["https://bsc-dataseed.binance.org/", "https://bsc-dataseed1.defibit.io/", "https://bsc-dataseed1.ninicoin.io/"]
+
+async function setupNetwork() {
+  const {ethereum}= window;
+    await ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: `0x${chainId.toString(16)}`,
+          chainName: `Binance Smart Chain Mainnet`,
+          nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18,
+          },
+          rpcUrls: nodes,
+          blockExplorerUrls: [`${blockExplorerUrl}/`],
+        },
+      ],
+    });
+    return true;
+}
+  return (
+    <div className="App">
+  <button onClick={setupNetwork}>Change Chain</button>
+    </div>
+  );
+}
+export default App;
